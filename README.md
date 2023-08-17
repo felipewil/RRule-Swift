7683df6ba7d0439484eea8eaceff27256cf3dec8
felipewil <felipewill2@gmail.com>
2023-08-17 14:56:56 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RRule-Swift
+
+A description of this package.
---
+++ Tests/RRule-SwiftTests/RRule_SwiftTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import RRule_Swift
+
+final class RRule_SwiftTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(RRule_Swift().text, "Hello, World!")
+    }
+}
